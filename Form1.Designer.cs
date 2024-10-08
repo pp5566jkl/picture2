3cef6dd94b7a74120e350e5a8ae9d9219a2ce9c2
STUST <STUST@STUST-42>
2024-10-08 14:46:01 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace picture2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox9 = new PictureBox();
+            pictureBox10 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(36, 21);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(249, 197);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(36, 263);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(249, 193);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(1007, 263);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(202, 152);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(791, 263);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(193, 152);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 3;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(571, 263);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(195, 152);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 4;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Location = new Point(340, 263);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(202, 152);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 5;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Location = new Point(1007, 21);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(202, 157);
+            pictureBox7.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox7.TabIndex = 6;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Location = new Point(791, 21);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(193, 157);
+            pictureBox8.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox8.TabIndex = 7;
+            pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.Location = new Point(571, 21);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(195, 157);
+            pictureBox9.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox9.TabIndex = 8;
+            pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            pictureBox10.Location = new Point(340, 21);
+            pictureBox10.Name = "pictureBox10";
+            pictureBox10.Size = new Size(202, 157);
+            pictureBox10.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox10.TabIndex = 9;
+            pictureBox10.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(117, 224);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 10;
+            button1.Text = "載入影像檔";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(117, 462);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 11;
+            button2.Text = "灰階化";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(727, 479);
+            label1.Name = "label1";
+            label1.Size = new Size(79, 15);
+            label1.TabIndex = 14;
+            label1.Text = "灰階位元平面";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(1099, 436);
+            label2.Name = "label2";
+            label2.Size = new Size(31, 15);
+            label2.TabIndex = 13;
+            label2.Text = "Bit 0";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(879, 436);
+            label3.Name = "label3";
+            label3.Size = new Size(31, 15);
+            label3.TabIndex = 14;
+            label3.Text = "Bit 1";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(666, 436);
+            label4.Name = "label4";
+            label4.Size = new Size(31, 15);
+            label4.TabIndex = 15;
+            label4.Text = "Bit 2";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(428, 436);
+            label5.Name = "label5";
+            label5.Size = new Size(31, 15);
+            label5.TabIndex = 16;
+            label5.Text = "Bit 3";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(1099, 192);
+            label6.Name = "label6";
+            label6.Size = new Size(31, 15);
+            label6.TabIndex = 17;
+            label6.Text = "Bit 4";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(879, 192);
+            label7.Name = "label7";
+            label7.Size = new Size(31, 15);
+            label7.TabIndex = 18;
+            label7.Text = "Bit 5";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(651, 192);
+            label8.Name = "label8";
+            label8.Size = new Size(31, 15);
+            label8.TabIndex = 19;
+            label8.Text = "Bit 6";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(428, 192);
+            label9.Name = "label9";
+            label9.Size = new Size(31, 15);
+            label9.TabIndex = 20;
+            label9.Text = "Bit 7";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1221, 524);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox10);
+            Controls.Add(pictureBox9);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox10;
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+namespace picture2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+                openFileDialog1.Filter = "圖像文件(JPeg, Gif, Bmp, etc.)|.jpg;*jpeg;*.gif;*.bmp;*.tif;*.tiff;*.png|所有文件(*.*)|*.*";
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    Bitmap MyBitmap = new Bitmap(openFileDialog1.FileName);
+                    this.pictureBox1.Image = MyBitmap;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "訊息顯示");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int Height = this.pictureBox1.Image.Height;
+                int Width = this.pictureBox1.Image.Width;
+                Bitmap newBitmap = new Bitmap(Width, Height);
+                Bitmap newBitmap0 = new Bitmap(Width, Height);
+                Bitmap newBitmap1 = new Bitmap(Width, Height);
+                Bitmap newBitmap2 = new Bitmap(Width, Height);
+                Bitmap newBitmap3 = new Bitmap(Width, Height);
+                Bitmap newBitmap4 = new Bitmap(Width, Height);
+                Bitmap newBitmap5 = new Bitmap(Width, Height);
+                Bitmap newBitmap6 = new Bitmap(Width, Height);
+                Bitmap newBitmap7 = new Bitmap(Width, Height);
+                Bitmap oldBitmap = (Bitmap)this.pictureBox1.Image;
+                Color pixel;
+                for (int x = 0; x < Width; x++)
+                    for (int y = 0; y < Height; y++)
+                    {
+                        pixel = oldBitmap.GetPixel(x, y);
+                        int r, g, b, Result = 0;
+                        int tmp;
+                        r = pixel.R;
+                        g = pixel.G;
+                        b = pixel.B;
+                        Result = (299 * r + 587 * g + 114 * b) / 1000;
+                        newBitmap.SetPixel(x, y, Color.FromArgb(Result, Result, Result));
+                        tmp = Result & 0x01;
+                        if (tmp != 0) newBitmap0.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                        else newBitmap0.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                        tmp = Result & 0x02;
+                        if (tmp != 0) newBitmap1.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                        else newBitmap1.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                        tmp = Result & 0x04;
+                        if (tmp != 0) newBitmap2.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                        else newBitmap2.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                        tmp = Result & 0x08;
+                        if (tmp != 0) newBitmap3.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                        else newBitmap3.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                        tmp = Result & 0x10;
+                        if (tmp != 0) newBitmap4.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                        else newBitmap4.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                        tmp = Result & 0x20;
+                        if (tmp != 0) newBitmap5.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                        else newBitmap5.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                        tmp = Result & 0x40;
+                        if (tmp != 0) newBitmap6.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                        else newBitmap6.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                        tmp = Result & 0x80;
+                        if (tmp != 0) newBitmap7.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                        else newBitmap7.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                      }
+                this.pictureBox2.Image = newBitmap;
+                this.pictureBox3.Image = newBitmap0;
+                this.pictureBox4.Image = newBitmap1;
+                this.pictureBox5.Image = newBitmap2;
+                this.pictureBox6.Image = newBitmap3;
+                this.pictureBox7.Image = newBitmap4;
+                this.pictureBox8.Image = newBitmap5;
+                this.pictureBox9.Image = newBitmap6;
+                this.pictureBox10.Image = newBitmap7;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "訊息顯示");
+            }
+        }
+    }
+}
